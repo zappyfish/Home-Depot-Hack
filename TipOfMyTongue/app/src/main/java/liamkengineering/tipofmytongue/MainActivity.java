520d99f37818d5f3bdea4dca32db7eb5bf27f039
zappyfish <liamkelly2015@gmail.com>
2017-02-16 22:08:09 -0600

    changes

--- TipOfMyTongue/app/src/main/java/liamkengineering/tipofmytongue/MainActivity.java
+++ TipOfMyTongue/app/src/main/java/liamkengineering/tipofmytongue/MainActivity.java
@@ -2,9 +2,9 @@ package liamkengineering.tipofmytongue;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,13 +15,15 @@ import android.widget.Toast;
 import com.google.gson.Gson;
 
 import java.io.BufferedReader;
-=======
 import org.json.*;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.util.*;
 import android.widget.Button;
 import android.widget.EditText;
 import com.google.gson.Gson;
->>>>>>> f02a9588bfb634947607e488f65a985e016e3fcc
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.HttpURLConnection;
@@ -34,12 +36,18 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class MainActivity extends AppCompatActivity {
-<<<<<<< HEAD
     //GoogleResults results;
     String apiKey;
     String engineKey;
     String searchURL;
     String result;
+    BufferedReader br;
+    URL url;
+    String status = "";
+    List<String> links = new ArrayList<>();
+    List<String> wordList = new ArrayList<>();
+    final String html = "<>p <span> foo </span> <em> bar <a> foobar </a> baz </em> </p>";
+    String qry="";
 
     TextView text;
     EditText searchEnter;
@@ -51,44 +59,90 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static final String GOOGLE_SEARCH_URL = "https://www.google.com/search";
-=======
-    public static final String[] IGNORED_WORDS = new String[] { "a", "an", "the", "thing" };
+    public static final String[] IGNORED_WORDS = new String[]{"a", "an", "the", "thing"};
     public static final Set<String> MY_SET = new HashSet<String>(Arrays.asList(IGNORED_WORDS));
->>>>>>> f02a9588bfb634947607e488f65a985e016e3fcc
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //setUp();
+        setUp();
         searchEnter = (EditText) findViewById(R.id.searchPhrase);
         Button searchBut = (Button) findViewById(R.id.searchButton);
-        text = (TextView)findViewById(R.id.test);
+        text = (TextView) findViewById(R.id.test);
 
         searchBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String searchURL = "http://www.google.com/#q=";
-                searchURL += searchEnter.getText().toString();
-                Toast t = Toast.makeText(MainActivity.this, searchURL, Toast.LENGTH_SHORT);
-                t.show();
-                Uri uri = Uri.parse(searchURL);
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
+                qry = searchEnter.getText().toString();
+                new AsyncTask<Void, Void, Void>() {
+
+                    @Override
+                    protected Void doInBackground(Void... params) {
+                        try {
+                            String key = apiKey;
+                            URL url = null;
+                            url = new URL("https://www.googleapis.com/customsearch/v1?q=" + qry + "&cx=012019511911701620333:ojaa7h2wxnc&key=" + key + "&alt=json");
+
+                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                            conn.setRequestMethod("GET");
+                            conn.setRequestProperty("Accept", "application/json");
+                            BufferedReader br = new BufferedReader(new InputStreamReader(
+                                    (conn.getInputStream())));
+
+                            String output;
+                            Log.v("out", "here");
+                            while ((output = br.readLine()) != null) {
+
+                                if (output.contains("\"link\": \"")) {
+                                    String link = output.substring(output.indexOf("\"link\": \"") + ("\"link\": \"").length(), output.indexOf("\","));
+                                    Log.v("links:", link);       //Will print the google search links
+                                    links.add(link);
+                                }
+                            }
+                            conn.disconnect();
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        } catch (ProtocolException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        for(int i = 0; i < links.size(); ++i) {
+                            Log.v("itearting: ", Integer.toString(i));
+                            try {
+                                Document doc = Jsoup.connect(links.get(i)).get();
+                                Elements paragraphs = doc.select("p");
+                                for (Element p : paragraphs) {
+                                    wordList.add(p.text());
+                                    Log.v("text: ", p.text());
+
+                                }
+                            }
+                            catch(Exception e) {
+
+                            }
+                        }
+                        return null;
+                    }
+
+                }.execute();
+
+
+
             }
         });
+
+
     }
 
-<<<<<<< HEAD
-
-=======
-    public static Set<String> searchKeywords (String search){
+    public static Set<String> searchKeywords(String search) {
         Set<String> keyWords = new HashSet<String>();
         StringTokenizer token = new StringTokenizer(search);
 
-        while (token.hasMoreTokens()){
+        while (token.hasMoreTokens()) {
             String keywordCandidate = token.nextToken().toLowerCase();
-            if (!MY_SET.contains(keywordCandidate)){
+            if (!MY_SET.contains(keywordCandidate)) {
                 keyWords.add(keywordCandidate);
             }
 
@@ -96,25 +150,13 @@ public class MainActivity extends AppCompatActivity {
 
         return keyWords;
     }
+}
 
-    public static void getResults() throws Exception {
-        String google = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=";
-        String search = "stackoverflow";
-        String charset = "UTF-8";
-
-        URL url = new URL(google + URLEncoder.encode(search, charset));
-        Reader reader = new InputStreamReader(url.openStream(), charset);
-        GoogleResults results = new Gson().fromJson(reader, GoogleResults.class);
-
-        // Show title and URL of 1st result.
-        System.out.println(results.getResponseData().getResults().get(0).getTitle());
-        System.out.println(results.getResponseData().getResults().get(0).getUrl());
-    }
 
 
     public static ArrayList<String> mostFreqKeywords(JSONObject googResults) {
         HashMap<String, Integer> map = new HashMap<String, Integer>();
-        JSONArray arr = googResults.getJSONArray("keyword"); // Insert key
+        JSONArray arr = googleResults.getJSONArray("keyword"); // Insert key
         for (int i = 0; i < arr.length(); i++) { // Map of word and number of occurrences
             map.put(arr[i], map.get[arr[i]] + 1);
         }
@@ -132,5 +174,4 @@ public class MainActivity extends AppCompatActivity {
         return results;
     }
 
->>>>>>> f02a9588bfb634947607e488f65a985e016e3fcc
-}
+
