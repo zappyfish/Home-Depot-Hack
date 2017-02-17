3837015222217866544b211453f5b0b672eeb893
zappyfish <liamkelly2015@gmail.com>
2017-02-16 22:50:45 -0600

    finished (with hackathon, anyway)

--- TipOfMyTongue/app/src/main/java/liamkengineering/tipofmytongue/MainActivity.java
+++ TipOfMyTongue/app/src/main/java/liamkengineering/tipofmytongue/MainActivity.java
@@ -33,10 +33,6 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class MainActivity extends AppCompatActivity {
-<<<<<<< HEAD
-=======
-
->>>>>>> 14cb57cf8eeb56ae8dac94bc1f1b30c3ac7fdab3
     //GoogleResults results;
     String apiKey;
     String engineKey;
@@ -48,10 +44,13 @@ public class MainActivity extends AppCompatActivity {
     List<String> links = new ArrayList<>();
     List<String> wordList = new ArrayList<>();
     final String html = "<>p <span> foo </span> <em> bar <a> foobar </a> baz </em> </p>";
-    String qry="";
+    String qry = "";
+    List<String> resultWords = new ArrayList<>();
 
     TextView text;
     EditText searchEnter;
+
+
 
     private void setUp() {
         apiKey = getString(R.string.apikey);
@@ -60,22 +59,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static final String GOOGLE_SEARCH_URL = "https://www.google.com/search";
-<<<<<<< HEAD
-    public static final String[] IGNORED_WORDS = new String[]{"a", "an", "the", "thing"};
-=======
 
 
     //list of most common words in english. Copied from https://gist.github.com/gravitymonkey/2406023
-    public static final String[] IGNORED_WORDS = new String[] {"the","of","and","a","to","in","is",
-            "you","that","it","he","was","for","on","are","as","with","his","they","I","at","be",
-            "this","have","from","or","one","had","by","word","but","not","what","all","were","we",
-            "when","your","can","said","there","use","an","each","which","she","do","how","their",
-            "if","will","up","other","about","out","many","then","them","these","so","some","her",
-            "would","make","like","him","into","has","look","two","more","write","go","see",
-            "no","way","could","people","my","than","first","been","call","who",
-            "its","now","find","down","day","did","get","come","made","may","part"};
+    public static final String[] IGNORED_WORDS = new String[]{"the", "of", "and", "a", "to", "in", "is",
+            "you", "that", "it", "he", "was", "for", "on", "are", "as", "with", "his", "they", "I", "at", "be",
+            "this", "have", "from", "or", "one", "had", "by", "word", "but", "not", "what", "all", "were", "we",
+            "when", "your", "can", "said", "there", "use", "an", "each", "which", "she", "do", "how", "their",
+            "if", "will", "up", "other", "about", "out", "many", "then", "them", "these", "so", "some", "her",
+            "would", "make", "like", "him", "into", "has", "look", "two", "more", "write", "go", "see",
+            "no", "way", "could", "people", "my", "than", "first", "been", "call", "who",
+            "its", "now", "find", "down", "day", "did", "get", "come", "made", "may", "part"};
 
->>>>>>> 14cb57cf8eeb56ae8dac94bc1f1b30c3ac7fdab3
     public static final Set<String> MY_SET = new HashSet<String>(Arrays.asList(IGNORED_WORDS));
 
     @Override
@@ -85,12 +80,21 @@ public class MainActivity extends AppCompatActivity {
         setUp();
         searchEnter = (EditText) findViewById(R.id.searchPhrase);
         Button searchBut = (Button) findViewById(R.id.searchButton);
+        Button displayBut = (Button) findViewById(R.id.display);
+        displayBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for(int i = 0; i<resultWords.size() && i<10; ++i) {
+                    Toast t = Toast.makeText(MainActivity.this, resultWords.get(i), Toast.LENGTH_SHORT);
+                    t.show();
+                }
+            }
+        });
         text = (TextView) findViewById(R.id.test);
 
         searchBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-<<<<<<< HEAD
                 qry = searchEnter.getText().toString();
                 new AsyncTask<Void, Void, Void>() {
 
@@ -125,21 +129,25 @@ public class MainActivity extends AppCompatActivity {
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
-                        for(int i = 0; i < links.size(); ++i) {
+                        for (int i = 0; i < links.size(); ++i) {
                             Log.v("itearting: ", Integer.toString(i));
                             try {
                                 Document doc = Jsoup.connect(links.get(i)).get();
                                 Elements paragraphs = doc.select("p");
                                 for (Element p : paragraphs) {
-                                    wordList.add(p.text());
-                                    Log.v("text: ", p.text());
+                                    List<String> temp = new ArrayList<>();
+                                    temp = searchKeywords(p.text());
+                                    for(int j = 0; j<temp.size(); ++j) {
+                                        wordList.add(temp.get(j));
+                                    }
+                                    Log.v("vals", p.text());
 
                                 }
-                            }
-                            catch(Exception e) {
+                            } catch (Exception e) {
 
                             }
-                        }
+                        }resultWords = mostFreqKeywords(wordList);
+
                         return null;
                     }
 
@@ -147,65 +155,38 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-=======
-                String searchURL = "http://www.google.com/#q=";
-                searchURL += searchEnter.getText().toString();
-                String keywords = searchEnter.getText().toString(), keywordFinal = "";
-                List<String> kw = searchKeywords(keywords);
-                for (String word : kw){
-                    keywordFinal += " " + word;
-                }
-                Toast t = Toast.makeText(MainActivity.this, keywordFinal, Toast.LENGTH_SHORT);
-                t.show();
-                Uri uri = Uri.parse(searchURL);
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
->>>>>>> 14cb57cf8eeb56ae8dac94bc1f1b30c3ac7fdab3
+
             }
         });
 
-<<<<<<< HEAD
 
     }
 
-    public static Set<String> searchKeywords(String search) {
-        Set<String> keyWords = new HashSet<String>();
-        StringTokenizer token = new StringTokenizer(search);
+    public static List<String> searchKeywords(String search) {
+            String[] array = {"the", "of", "and", "a", "to", "in", "is",
+                    "you", "that", "it", "he", "was", "for", "on", "are", "as", "with", "his", "they", "I", "at", "be",
+                    "this", "have", "from", "or", "one", "had", "by", "word", "but", "not", "what", "all", "were", "we",
+                    "when", "your", "can", "said", "there", "use", "an", "each", "which", "she", "do", "how", "their",
+                    "if", "will", "up", "other", "about", "out", "many", "then", "them", "these", "so", "some", "her",
+                    "would", "make", "like", "him", "into", "has", "look", "two", "more", "write", "go", "see",
+                    "no", "way", "could", "people", "my", "than", "first", "been", "call", "who", "lets", "thing",
+                    "its", "now", "find", "down", "day", "did", "get", "come", "made", "may", "part"};
+            List<String> list = Arrays.asList(array);
+            List<String> keywords = new ArrayList<String>();
 
-        while (token.hasMoreTokens()) {
-            String keywordCandidate = token.nextToken().toLowerCase();
-            if (!MY_SET.contains(keywordCandidate)) {
-                keyWords.add(keywordCandidate);
-=======
-    public static List<String> searchKeywords (String search){
-        String [] array = {"the","of","and","a","to","in","is",
-                "you","that","it","he","was","for","on","are","as","with","his","they","I","at","be",
-                "this","have","from","or","one","had","by","word","but","not","what","all","were","we",
-                "when","your","can","said","there","use","an","each","which","she","do","how","their",
-                "if","will","up","other","about","out","many","then","them","these","so","some","her",
-                "would","make","like","him","into","has","look","two","more","write","go","see",
-                "no","way","could","people","my","than","first","been","call","who", "lets", "thing",
-                "its","now","find","down","day","did","get","come","made","may","part"};
-        List<String> list = Arrays.asList(array);
-        List<String> keywords = new ArrayList<String>();
+            StringTokenizer tokenizer = new StringTokenizer(search);
 
-        StringTokenizer tokenizer = new StringTokenizer(search);
+            while (tokenizer.hasMoreTokens()) {
+                String word = tokenizer.nextToken();
+                if (!list.contains(word)) {
+                    keywords.add(word);
 
-        while (tokenizer.hasMoreTokens()){
-            String word = tokenizer.nextToken();
-            if (!list.contains(word)){
-                keywords.add(word);
+                }
 
->>>>>>> 14cb57cf8eeb56ae8dac94bc1f1b30c3ac7fdab3
             }
-
+            return keywords;
         }
-        return keywords;
-    }
-}
 
-<<<<<<< HEAD
-=======
     /*public static void getResults() throws Exception {
         String google = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=";
         String search = "stackoverflow";
@@ -219,20 +200,23 @@ public class MainActivity extends AppCompatActivity {
         System.out.println(results.getResponseData().getResults().get(0).getTitle());
         System.out.println(results.getResponseData().getResults().get(0).getUrl());
     }*/
->>>>>>> 14cb57cf8eeb56ae8dac94bc1f1b30c3ac7fdab3
 
 
     public static ArrayList<String> mostFreqKeywords(List<String> googResults) {
         HashMap<String, Integer> map = new HashMap<String, Integer>();
-<<<<<<< HEAD
-        JSONArray arr = googleResults.getJSONArray("keyword"); // Insert key
-        for (int i = 0; i < arr.length(); i++) { // Map of word and number of occurrences
-            map.put(arr[i], map.get[arr[i]] + 1);
-=======
-        for (int i = 0; i < googResults.size(); ++i) {
-            map.put(googResults.get(i), map.get(googResults.get(i)) + 1);
->>>>>>> 14cb57cf8eeb56ae8dac94bc1f1b30c3ac7fdab3
+        if(googResults!=null) {
+            for (int i = 0; i < (int)googResults.size(); ++i) {
+                if(googResults.get(i)!=null) {
+                    if(!map.containsKey(googResults.get(i)))
+                    {
+                        map.put(googResults.get(i), 1);
+                    }
+                    else
+                    map.put(googResults.get(i), (int) map.get(googResults.get(i)) + 1);
+                }
+            }
         }
+
         /*
         try {
             JSONArray arr = googResults.getJSONArray("hi"); // Insert key
@@ -244,22 +228,19 @@ public class MainActivity extends AppCompatActivity {
         }
         */
 
-        Object[] a = map.entrySet().toArray();
-        Arrays.sort(a, new Comparator() { // Sort array by value
-            public int compare(Object o1, Object o2) {
-                return ((Map.Entry<String, Integer>) o2).getValue()
-                        .compareTo(((Map.Entry<String, Integer>) o1).getValue());
+            Object[] a = map.entrySet().toArray();
+            Arrays.sort(a, new Comparator() { // Sort array by value
+                public int compare(Object o1, Object o2) {
+                    return ((Map.Entry<String, Integer>) o2).getValue()
+                            .compareTo(((Map.Entry<String, Integer>) o1).getValue());
+                }
+            });
+            ArrayList<String> results = new ArrayList<String>();
+            for (int i = 0; i<a.length; ++i) { // Add top ten most freq words
+                results.add(a[i].toString());
             }
-        });
-        ArrayList<String> results = new ArrayList<String>();
-        for (int i = 0; i < 10; ++i) { // Add top ten most freq words
-            results.add(a[i].toString());
-        }
+
         return results;
+
     }
-<<<<<<< HEAD
-
-
-=======
 }
->>>>>>> 14cb57cf8eeb56ae8dac94bc1f1b30c3ac7fdab3
