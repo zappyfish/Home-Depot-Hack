cb5f75b84875a0dc15498a2721048865f1866fda
zappyfish <liamkelly2015@gmail.com>
2017-02-16 18:55:47 -0600

    search

--- TipOfMyTongue/app/src/main/java/liamkengineering/tipofmytongue/MainActivity.java
+++ TipOfMyTongue/app/src/main/java/liamkengineering/tipofmytongue/MainActivity.java
@@ -5,6 +5,13 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.gson.Gson;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.net.URLEncoder;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,7 +20,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         EditText searchEnter = (EditText) findViewById(R.id.searchPhrase);
-
         Button searchBut = (Button) findViewById(R.id.searchButton);
+
+
+    }
+
+    public static void getResults() throws Exception {
+        String google = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=";
+        String search = "stackoverflow";
+        String charset = "UTF-8";
+
+        URL url = new URL(google + URLEncoder.encode(search, charset));
+        Reader reader = new InputStreamReader(url.openStream(), charset);
+        GoogleResults results = new Gson().fromJson(reader, GoogleResults.class);
+
+        // Show title and URL of 1st result.
+        System.out.println(results.getResponseData().getResults().get(0).getTitle());
+        System.out.println(results.getResponseData().getResults().get(0).getUrl());
     }
 }
