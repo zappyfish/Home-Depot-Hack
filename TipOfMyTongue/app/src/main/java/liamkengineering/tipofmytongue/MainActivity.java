9ddf7c2174fe42da490a2b4c6c78567370042dc7
zappyfish <liamkelly2015@gmail.com>
2017-02-16 18:49:41 -0600

    added search field

--- TipOfMyTongue/app/src/main/java/liamkengineering/tipofmytongue/MainActivity.java
+++ TipOfMyTongue/app/src/main/java/liamkengineering/tipofmytongue/MainActivity.java
@@ -2,6 +2,8 @@ package liamkengineering.tipofmytongue;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText searchEnter = (EditText) findViewById(R.id.searchPhrase);
+
+        Button searchBut = (Button) findViewById(R.id.searchButton);
     }
 }
