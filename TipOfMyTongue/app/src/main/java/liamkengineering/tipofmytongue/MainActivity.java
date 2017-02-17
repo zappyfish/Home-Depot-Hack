a408a45949dba8808640aa6dd450280379a8cd4e
Roger Li <roger.g.li@vanderbilt.edu>
2017-02-16 19:47:46 -0600

    implement rough version of JSONObject parser and returns most common results

--- TipOfMyTongue/app/src/main/java/liamkengineering/tipofmytongue/MainActivity.java
+++ TipOfMyTongue/app/src/main/java/liamkengineering/tipofmytongue/MainActivity.java
@@ -2,6 +2,8 @@ package liamkengineering.tipofmytongue;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import org.json.*;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public ArrayList<String> mostFreqKeywords(set<String> tokens, JSONObject googResults) {
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+         //each key in the jsonobject
+        JSONArray arr = googResults.getJSONArray("keyword"); //insert key
+        for (int i = 0; i < arr.length(); i++) {
+            map.put(arr[i], map.get[arr[i]] + 1);
+        }
+        Object[] a = map.entrySet().toArray();
+        Arrays.sort(a, new Comparator() {
+            public int compare(Object o1, Object o2) {
+                return ((Map.Entry<String, Integer>) o2).getValue()
+                        .compareTo(((Map.Entry<String, Integer>) o1).getValue());
+            }
+        });
+        ArrayList<String> results = new ArrayList<String>();
+        for (int i = 0; i < 10; ++i) {
+            results.add(a[i].getValue());
+        }
+        return results;
+    }
+
 }
